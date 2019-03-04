0cbd356ae7c53418deaf9d32b20e1669a39513e3
Verf <verf@protonmail.com>
2019-03-04 15:19:46 +0800

    change covec to torchplp

--- setup.py
+++ setup.py
@@ -2,30 +2,31 @@
 # coding: utf-8
 from setuptools import setup, find_packages
 
+VERSION = '0.1'
+REQUIREMENTS = [
+        'numpy',
+        'torch',
+        'gensim',
+        ]
 
 def readme():
     with open('README.md') as f:
         return f.read()
 
-
 setup(
-    name='covec',
-    version='0.11',
-    author="NIPC-DL",
-    author_email="verf@protonmail.com",
-    description="Python Code Processor for Machine Learning",
+    name='torchplp',
+    version=VERSION,
+    author='NIPC-DL',
+    author_email='verf@protonmail.com',
+    url="https://github.com/NIPC-DL/torchplp",
+    description='datasets and process methods for program source code analysis,
     long_description=readme(),
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    url="https://github.com/NIPC-DL/Covec",
-    license='MIT',
-    packages=find_packages(),
-    install_requires=[
-        'clang',
-        'requests',
-        'gensim',
-    ],
+    packages=find_packages(exclude=('test',)),
+    install_requires=REQUIREMENTS
 )
